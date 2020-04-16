30706443676a6dcd3e9f24e85b99d06da0061d66
bkawila <thanawat.kawi@gmail.com>
2020-04-16 13:34:49 +0700

    add new string

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -5,6 +5,7 @@ namespace PLREPORT.Models
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
+        public string name {get; set;}
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
